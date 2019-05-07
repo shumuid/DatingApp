2288405f193d63bf36f64d78aa27d90b334e5230
Costin Somodea <costin.somodea@yahoo.com>
2019-05-07 17:43:43 +0300

    end of section 16

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -31,6 +31,8 @@ import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { ListsResolver } from './_resolvers/lists.resolver';
+import { MessagesResolver } from './_resolvers/messages.resolver';
+import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -47,6 +49,7 @@ export function tokenGetter() {
       MemberDetailComponent,
       MemberCardComponent,
       MemberEditComponent,
+      MemberMessagesComponent,
       PhotoEditorComponent,
       ListsComponent,
       MessagesComponent,
@@ -83,6 +86,7 @@ export function tokenGetter() {
       MemberListResolver,
       MemberEditResolver,
       ListsResolver,
+      MessagesResolver,
       PreventUnsavedChanges
    ],
    bootstrap: [
